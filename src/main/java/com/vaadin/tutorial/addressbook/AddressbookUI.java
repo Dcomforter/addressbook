fa7a1c76c879a3365e01ef525e5f979e0f17f135
Israel Okuneye <104809559+Dcomforter@users.noreply.github.com>
2024-04-11 14:12:55 -0500

    Update AddressbookUI.java

--- src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
+++ src/main/java/com/vaadin/tutorial/addressbook/AddressbookUI.java
@@ -71,7 +71,7 @@ public class AddressbookUI extends UI {
          */
         newContact.addClickListener(e -> contactForm.edit(new Contact()));
 
-        filter.setInputPrompt("Filter contacts by first name....");
+        filter.setInputPrompt("Filter contacts by firstname....");
         filter.addTextChangeListener(e -> refreshContacts(e.getText()));
 
         contactList
